6c4f728e00de01b3fa590999fa1694a3df9cbbe8
Harsh Rai <133752337+Harshrai13@users.noreply.github.com>
2026-02-27 13:22:19 +0530

    Create sample_data.sql

---
+++ database/sample_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Officers (name, rank, station)
+VALUES ('Rajesh Singh', 'Inspector', 'Bhopal Central');
+
+INSERT INTO Crime_Category (category_name)
+VALUES ('Theft'), ('Fraud'), ('Cyber Crime');
